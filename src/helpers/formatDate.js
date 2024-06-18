1c8c53e36b81447faaf12400fd45423489f43b05
Roman Dakaev <romanov777123@gmail.com>
2024-06-18 09:44:04 +0300

    git init

---
+++ src/helpers/formatDate.js
@@ -0,0 +1,9 @@
+export const formatData = (date) => {
+  const options = {
+    weekday: "long",
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  };
+  return date.toLocaleDateString("en-US", options);
+};
